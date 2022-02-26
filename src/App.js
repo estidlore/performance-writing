3f70b65d990c0be83ae0763fb80c92ec3bb38eab
Estid Felipe Lozano Reyes <estid_l@hotmail.com>
2022-02-26 01:27:26 -0500

    Added writing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import text from './Text.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const wordsCount = text.text.reduce((prev, curr) => prev + curr.split(' ').length, 0);
+
+const Paragraph = ({text}) => (
+  <p className="py-2 my-0 px-2 bg-light">{
+    text.split(' ').map(e => <span>{e + ' '}</span>)
+  }
+  </p>
+);
+
+const App = () => (
+  <div className="bg-light py-5 px-2">
+    <div className="mw-100 mx-auto" style={{width: '700px'}}>
+      <h1 className="text-center mt-4 mb-3">Performance</h1>
+      {
+        text.text.map(paragraph => <Paragraph text={paragraph} />)
+      }
+      <hr />
+      <div className="d-flex justify-content-between">
+        <span>By Estid Felipe Lozano Reyes</span>
+        <span>Words Count: {wordsCount}</span>
+      </div>
     </div>
-  );
-}
+    <img className="position-fixed top-50 mx-5 d-sm-none d-lg-block start-0" src="pencil.png" width="50" />
+    <img className="position-fixed top-50 mx-5 d-sm-none d-lg-block end-0" src="pencil.png" width="50" />
+  </div>
+);
 
 export default App;
